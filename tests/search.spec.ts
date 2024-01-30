0c7bbb323eae5610e5ff381c2d91fae100658b19
Bayron_Chavarria <bayronchava@gmail.com>
2024-01-30 12:42:31 -0500

    Se corrige los errores

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/'); //Se encuentra global en config
 });
 
 test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
-  await page.getByRole('button').click();
+  await page.getByRole('button', {name: "Search"}).click();
 
   await page.getByPlaceholder('Search docs').click();
 
   await page.getByPlaceholder('Search docs').fill('hascontent');
 
-  expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+  await expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
 
-  expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for hascontent');
+  await expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for "hascontent"');
 
 })
 
@@ -26,7 +26,7 @@ test('Limpiar el input de busqueda', async ({ page }) => {
 
   await searchBox.fill('somerandomtext');
 
-  await expect(searchBox).toHaveText('somerandomtext');
+  await expect(searchBox).toHaveAttribute('value',"somerandomtext");
 
   await page.getByRole('button', { name: 'Clear the query' }).click();
 
@@ -34,7 +34,7 @@ test('Limpiar el input de busqueda', async ({ page }) => {
 });
 
 test('Realizar una busqueda que genere al menos tenga un resultado', async ({ page }) => {
-  await page.getByRole('button', { name: 'Search ' }).click();
+  await page.getByRole('button', { name: 'Search' }).click();
 
   const searchBox = page.getByPlaceholder('Search docs');
 
@@ -42,11 +42,29 @@ test('Realizar una busqueda que genere al menos tenga un resultado', async ({ pa
 
   await page.getByPlaceholder('Search docs').fill('havetext');
 
-  expect(searchBox).toHaveText('havetext');
+  await expect(searchBox).toHaveAttribute("value",'havetext');
 
   // Verity there are sections in the results
   await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
   const numberOfResults = await page.locator('.DocSearch-Dropdown-Container section').count();
   await expect(numberOfResults).toBeGreaterThan(0);
+
+});
+
+test.only('Dar click a la primera opción de búsqueda', async ({ page }) => {
+
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  const searchBox1 = page.getByPlaceholder('Search docs');
+
+  await searchBox1.click();
+
+  await page.getByPlaceholder('Search docs').fill('havetext');
+
+  await expect(searchBox1).toHaveAttribute("value",'havetext');
+
+
+  await page.getByRole('link', { name: 'toHaveText​ LocatorAssertions' }).click();
+  await expect(page).toHaveURL('https://playwright.dev/docs/api/class-locatorassertions#locator-assertions-to-have-text');
 
 });
